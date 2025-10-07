50d9787a95dbe8ec9cc7148108c4443f68d0902d
Erencan Pelin <erencan.pelin@gmail.com>
2025-10-07 22:44:20 +1100

    Fix ServiceLocator function annotations

--- Core/ServiceLocator.cs
+++ Core/ServiceLocator.cs
@@ -20,9 +20,6 @@ namespace UnityUtils.Core
         /// <summary>
         /// Registers a service
         /// </summary>
-        /// <param name="service"></param>
-        /// <typeparam name="T"></typeparam>
-        /// <exception cref="Exception">Throws if that service is already registered</exception>
         public void Register<T>(T service) where T : class
         {
             var type = typeof(T);
@@ -33,8 +30,6 @@ namespace UnityUtils.Core
         /// <summary>
         /// Unregisters an existing service
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <exception cref="Exception">Throws if that service is not already registered</exception>
         public void Unregister<T>() where T : class
         {
             var type = typeof(T);
@@ -46,8 +41,7 @@ namespace UnityUtils.Core
         /// Returns the instance of the service based on the given service type
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        /// <exception cref="Exception"></exception>
+        /// <returns>The instance of the service of the given type</returns>
         public T Resolve<T>() where T : class
         {
             var type = typeof(T);
@@ -58,8 +52,6 @@ namespace UnityUtils.Core
         /// <summary>
         /// Returns true if that service is registered already
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
         public bool IsRegistered<T>() where T : class
         {
             return _services.ContainsKey(typeof(T));
